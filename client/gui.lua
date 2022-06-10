2f86f6e801e51402b73ca108313c078a66ae0cc0
mysty <74386804+mystyleigh@users.noreply.github.com>
2022-06-09 21:33:36 -0400

    Update gui.lua
    added function to sort keys in the array so skills are always displayed alphabetically

--- client/gui.lua
+++ client/gui.lua
@@ -1,5 +1,20 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 
+-- sort keys into an array then iterate the sorted array and return key & value
+local function pairsByKeys (t, f)
+	local a = {}
+	for n in pairs(t) do table.insert(a, n) end
+	table.sort(a, f)
+	local i = 0      -- iterator variable
+	local iter = function ()   -- iterator function
+		i = i + 1
+		if a[i] == nil then return nil
+		else return a[i], t[a[i]]
+		end
+	end
+	return iter
+end
+
 local function createSkillMenu()
     skillMenu = {}
     skillMenu[#skillMenu + 1] = {
@@ -8,7 +23,7 @@ local function createSkillMenu()
         isMenuHeader = true,
         icon = 'fas fa-chart-simple'
     }
-    for k,v in pairs(Config.Skills) do
+    for k,v in pairsByKeys(Config.Skills) do
         skillMenu[#skillMenu + 1] = {
             header = ''.. k .. '',
             txt = ''..v['Current']..'%',
@@ -20,6 +35,7 @@ local function createSkillMenu()
             }
         }
     end
+	
     exports['qb-menu']:openMenu(skillMenu)
 end
     
